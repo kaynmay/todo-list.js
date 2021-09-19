1bd972e87a11e30f2808240f64065f2002b18344
Kayla <kaynmay@gmail.com>
2021-09-19 18:30:07 -0500

    add navbar functionality

--- src/functionBased/pages/About.js
+++ src/functionBased/pages/About.js
@@ -6,8 +6,8 @@ import { default as SinglePage } from './SinglePage'
 const About = (props) => {
   const { url, path } = useRouteMatch()
   return (
-    <div>
-      <ul>
+    <div className='about__content'>
+      <ul className='about__list'>
         <li>
           <Link to={`${url}/about-app`}>About App</Link>
         </li>
